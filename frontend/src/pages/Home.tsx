ffd1423a94b64691f65cd147164405a09449fe26
Jacksgithubaccoun <158605432+Jacksgithubaccoun@users.noreply.github.com>
2025-07-08 14:14:24 -0500

    Update Home.tsx

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -70,7 +70,7 @@ const Home: React.FC = () => {
 
       console.log('Sending feed URL:', url);
 
-      const res = await fetch('/api/fetch-feed', {
+      const res = await fetch('./fetch-articles', {
         method: 'POST',
         body: JSON.stringify({ url }),
         headers: { 'Content-Type': 'application/json' },
