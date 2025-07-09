2663865fbca01a8a364a474d8a56bf3b17bcb4b6
Jacksgithubaccoun <158605432+Jacksgithubaccoun@users.noreply.github.com>
2025-07-09 15:08:43 -0500

    Update Home.tsx

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -206,8 +206,7 @@ const Home: React.FC = () => {
   return (
     <>
       <MatrixRain />
-      <main style={styles.container     position: 'relative',  // Needed for z-index to work on this container
-    zIndex: 10,}>
+      <main style={{ ...styles.container, position: 'relative', zIndex: 10 }}>
         <h1 style={styles.title}>Jaggregator</h1>
 
         <FeedsManager
