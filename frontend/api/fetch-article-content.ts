2e290f0a862142eb01a571177ee35547f535fb32
Jacksgithubaccoun <158605432+Jacksgithubaccoun@users.noreply.github.com>
2025-07-10 15:22:21 -0500

    Update fetch-article-content.ts

--- frontend/api/fetch-article-content.ts
+++ frontend/api/fetch-article-content.ts
@@ -38,4 +38,4 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   } catch (error) {
     res.status(500).json({ error: 'Error fetching or parsing article' });
   }
-}
+}"
